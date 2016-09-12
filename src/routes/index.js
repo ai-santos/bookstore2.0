bf581d4db65d9ea10f69d52800ff811f76949d71
Aileen Santos <asantos3026@gmail.com>
2016-09-12 15:02:58 -0700

    started authentication WIP

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,20 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (request, response) => {
-  response.render('index')
+  request.session.times = (request.session.times || 0) + 1
+  response.render('index', {
+    session: request.session
+  }) 
 })
 
 router.get('/signup', (request, response) => {
   response.render('users/signup')
 })
+
+router.post('/signup', (request, response) => {
+  response.json(request.body)
+})
+
 
 router.get('/login', (request, response) => {
   response.render('users/login')
