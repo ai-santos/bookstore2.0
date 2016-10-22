6cd925db2205e6a0fe364cd8253dbbc63b49e37b
Aileen Santos <asantos3026@gmail.com>
2016-10-22 00:50:42 -0700

    added postgres database to database.js file

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import password from './password'
 const pgp = require('pg-promise')();
-const connectionString = process.env.DATABASE_URL || `postgres://${process.env.USER}@localhost:5432/bookstore2.0`
+const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/bookstore2.0`
 const db = pgp(connectionString)
 
 const getAllGenres = () => {
